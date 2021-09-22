4156524de9c85bb97ffa4ada401a43f219f8f063
brianhung7 <brian.texts@gmail.com>
2021-09-22 16:06:17 -0700

    footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,14 +1,33 @@
 import React from 'react';
-import { AiFillGithub, AiFillInstagram, AiFillLinkedin } from 'react-icons/ai';
+import { AiFillGithub, AiOutlineMail, AiFillLinkedin } from 'react-icons/ai';
 
 import { SocialIcons } from '../Header/HeaderStyles';
 import { CompanyContainer, FooterWrapper, LinkColumn, LinkItem, LinkList, LinkTitle, Slogan, SocialContainer, SocialIconsContainer } from './FooterStyles';
 
 const Footer = () => {
   return (
-    <div>
-      Footer
-    </div>
+    <FooterWrapper>
+      <LinkList>
+        <LinkColumn>
+          <LinkTitle>Contact</LinkTitle>
+          <LinkItem href="mailto:brian.texts@gmail.com">brian.texts@gmail.com</LinkItem>
+        </LinkColumn>
+      </LinkList>
+      <SocialIconsContainer>
+        <CompanyContainer>
+          <Slogan>Always enhancing, pioneering, and innovating</Slogan>
+        </CompanyContainer>
+        <SocialIcons href="github.com">
+          <AiFillGithub size="3rem" />
+        </SocialIcons>
+        <SocialIcons href="linkedin.com">
+          <AiFillLinkedin size="3rem" />
+        </SocialIcons>
+        <SocialIcons href="brian_van_pham@yahoo.com">
+          <AiOutlineMail size="3rem" />
+        </SocialIcons>
+      </SocialIconsContainer>
+    </FooterWrapper >
   );
 };
 
